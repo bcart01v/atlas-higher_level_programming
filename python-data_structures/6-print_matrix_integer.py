b67d13e495147df5f4444ec475b9db3e82eb1db6
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-01-05 17:50:04 -0600

    Corrected output to remove trailing spaces causing extra characters and checker don't like that

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,9 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end= ' ')
+        for i, elem in enumerate(row):
+            if i < len(row) - 1:
+                print("{:d}".format(elem), end=' ')
+            else:
+                print("{:d}".format(elem), end='')
         print()
