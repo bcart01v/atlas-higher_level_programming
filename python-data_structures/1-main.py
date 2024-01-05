66d627fc2703858c296c0841319df617358b8478
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-01-05 16:22:23 -0600

    Project 1

---
+++ python-data_structures/1-main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+element_at = __import__('1-element_at').element_at
+
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+
+my_list = [5, 9, 3, 10, 20]
+idx = 2
+print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+
+my_list = [1, 2, 3]
+idx = 7
+print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
