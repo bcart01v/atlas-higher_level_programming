2240284c44bede41337e6b25ad8af10a99a5dd35
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-01-07 01:14:19 -0600

    return format correciton, added none return specifier

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,8 +2,8 @@
 
 def multiple_returns(sentence):
     if len(sentence) <= 0:
-        return
+        return None, None
     first = sentence[0:1]
     count = len(sentence)
 
-    return(count, first)
+    return count, first
