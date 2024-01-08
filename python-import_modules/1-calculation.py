b383797df660e452af41494d74b4540354303bb6
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-01-08 14:39:11 -0600

    Try this again

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -2,16 +2,9 @@
 
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-
     a = 10
     b = 5
-
-    add_total = add(a, b)
-    sub_total = sub(a, b)
-    mul_total = mul(a, b)
-    div_total = div(a, b)
-
-    print("{} + {} = {}".format(a, b, add_total))
-    print("{} - {} = {}".format(a, b, sub_total))
-    print("{} * {} = {}".format(a, b, mul_total))
-    print("{} / {} = {}".format(a, b, div_total))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
