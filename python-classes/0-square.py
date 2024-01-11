934483df5e96543efc07c01a1e75983f89bf00fd
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-01-11 16:35:05 -0600

    Added definition

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
-def square():
+"This is a Square class it's a class that contains a Square"
+
+class Square:
     """This is an empty class Square that defines a square"""
     pass
