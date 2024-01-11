02e94494a7de4eb5917610eb771c3fa5b1fcd574
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-01-11 16:41:11 -0600

    Defining a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"This class is a square that can be defined"
+
+class Square: 
+    """This is a class that defines a square"""
+    def __init__(self, size=0):
+        """This is a function that defines the size of the square"""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0: 
+            raise ValueError("size must be >= 0")
