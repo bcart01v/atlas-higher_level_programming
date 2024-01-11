3d6f7fdef6265bae8e6f7543aeef3413f31a70da
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-01-11 16:42:25 -0600

    Pycodestyle corrections

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,12 +2,13 @@
 
 "This class is a square that can be defined"
 
-class Square: 
+
+class Square:
     """This is a class that defines a square"""
     def __init__(self, size=0):
         """This is a function that defines the size of the square"""
         self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        if size < 0: 
+        if size < 0:
             raise ValueError("size must be >= 0")
