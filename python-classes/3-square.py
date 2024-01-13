798d6c3a2c1e4d11959b3ef78e0617f2805b6ebe
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-01-12 21:25:38 -0600

    Added an area attribute to the Square class

---
+++ python-classes/3-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"Another Square Class"
+
+
+class Square:
+    "This is a class that defines a sqauare"
+    def __init__(self, size = 0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        
+    def area (self):
+        return self.__size ** 2
