5884453e2c0a4f07ef542020870a8b41532b7cbe
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-01-15 01:15:59 -0600

    Spacing counts

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module 1-rectangle"""
 
+
 class Rectangle:
     """Rectangle class with width and height"""
     def __init__(self, width=0, height=0):
@@ -19,7 +20,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must be >=0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -33,6 +34,6 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height must be >=0")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
