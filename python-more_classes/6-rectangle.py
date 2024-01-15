ec765202d526e4c5e2e980ef8da7c8bb7cb5232d
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-01-15 02:18:51 -0600

    Is that what it meant?

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle:
     """Rectangle class with width and height"""
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Initialization with optional width and height"""
         self.width = width
