b1576a01f17921f95af2f2a9438095291bdeb639
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-01-20 14:33:36 -0600

    Exact same object

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 
-""" A simple Lookup function """
+""" 
+Module: 0-lookup.py
+
+Functions:
+    lookup(obj) - Returns a list containing the names of all the attributes and
+    methods of the specified object.
+
+Usage:
+    Used to return a list of attributes and methods of an object
+
+"""
 
 def lookup(obj):
     """ Returns the list of available attributes and methods of an object
---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+class IsSameClass:
+    """Class to check if an object is the same as the specified class
+    
+    Functions:
+        is_same_class(obj, a_class) - Returns True if the object is exactly an
+        instance of the specified class; otherwise False.
+    
+    Usage:
+        Used to check if an object is the same as the specified class
+    """ 
+
+    def is_same_class(obj, a_class):
+        """ Checks if an object is the same as the specified class
+        """
+        return type(obj) is a_class
