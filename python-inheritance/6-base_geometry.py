cb7b5a7150fb1ce33a3cee7269c4c899fd73848d
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-01-22 12:02:07 -0600

    We need to raise an exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""" Module: 6-base_geometry.py
+
+    This module contains the BaseGeometry class
+
+    Classes:
+        BaseGeometry - It's an empty class.
+
+    Functions:
+        def area(self) - Raises an exception.
+
+    Usage:
+        Only raises an exception (for now)
+
+"""
+class BaseGeometry:
+    """ BaseGeometry class
+
+        No longer an empty class, we have area now.
+        Except the area only returns an exception.
+
+
+    """
+    def area(self):
+        """ area function
+
+        It's a public instance method.
+
+    """
+        raise Exception("area() is not implemented")
