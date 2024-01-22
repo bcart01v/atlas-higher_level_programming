042903726167a5d23413a66ce91b6b7b8f84fded
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-01-22 17:38:14 -0600

    I am stupid

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,6 +2,7 @@
 
 """Class to JSON module. Returns the dictionary description with simple data"""
 
+
 def class_to_json(obj):
     """Returns the dictionary description with simple data structure"""
     return obj.__dict__
