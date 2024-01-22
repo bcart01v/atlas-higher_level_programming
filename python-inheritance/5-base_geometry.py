ff4dd767740bf08557bd79a5a7e6fedc3e9edd75
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-01-22 11:29:36 -0600

    Created an empty class

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+""" Module: 5-base_geometry.py
+    This module contains the BaseGeometry class
+
+    Classes:
+        BaseGeometry - It's an empty class.
+
+    Functions:
+        None
+
+    Usage:
+        We don't know yet
+"""
+class BaseGeometry:
+    """ Class: BaseGeometry
+        Just an empty Class right now.
+
+        Methods:
+            None yet
+    """
+    pass
