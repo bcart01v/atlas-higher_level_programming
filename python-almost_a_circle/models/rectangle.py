a1f3faea9980615c4ad2ade254cd76bcee3029c5
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-01-26 17:39:11 -0600

    Created Base, Init, and Rectangle. Gave rectangles some properties. Need to commit more often

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+""" Rectangle class """
+from models.base import Base
+
+def __init__(self, width, height, x=0, y=0, id=None):
+    """ Constructor """
+    super().__init__(id)
+    self.__width = width
+    self.__height = height
+    self.__x = x
+    self.__y = y
+
+@property
+def width(self):
+    """ Getter """
+    return self.__width
+
+@width.setter
+def width(self, value):
+    """ Setter """
+    # This will probably need validation of some sort.
+    self.__width = value
+
+@property
+def height(self);
+    """ Getter """
+    return (self.__height)
+
+@height.setter
+def height(self, value):
+    """ Setter """
+    # This will probably need validation of some sort
+    self.__height = value
