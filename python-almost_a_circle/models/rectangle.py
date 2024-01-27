e4df6965efd7ccd693e50bafb36129311826bd04
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-01-26 22:44:14 -0600

    Created custom STR function

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -77,6 +77,16 @@ class Rectangle(Base):
                 print("#", end="")
             print()
 
+    def __str__(self):
+        """ This function returns the a custom version of
+        what str would return.
+
+        Returns:
+            [Rectangle] (id) x/y width/height
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, \
+            self.__width, self.__height)
+
     @property
     def width(self):
         """Getter for width"""
