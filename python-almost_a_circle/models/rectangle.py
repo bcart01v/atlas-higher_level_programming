e1c26eb8c4798a1ae444e69fd6aa9834c4e7d9cf
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-01-26 19:47:53 -0600

    Made indentation corrections (Keep forggeting about that) And corected the x and y values

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,54 +1,58 @@
 #!/usr/bin/python3
 
-""" Rectangle class """
+"""Rectangle class"""
 from models.base import Base
 
-def __init__(self, width, height, x=0, y=0, id=None):
-    """ Constructor """
-    super().__init__(id)
-    self.__width = width
-    self.__height = height
-    self.__x = x
-    self.__y = y
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Constructor"""
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
-@property
-def width(self):
-    """ Getter """
-    return self.__width
+    @property
+    def width(self):
+        """Getter for width"""
+        return self.__width
 
-@width.setter
-def width(self, value):
-    """ Setter """
-    # This will probably need validation of some sort.
-    self.__width = value
+    @width.setter
+    def width(self, value):
+        """Setter for width"""
+        # Add validation if needed
+        self.__width = value
 
-@property
-def height(self):
-    """ Getter """
-    return self.__height
+    @property
+    def height(self):
+        """Getter for height"""
+        return self.__height
 
-@height.setter
-def height(self, value):
-    """ Setter """
-    # This will probably need validation of some sort
-    self.__height = value
+    @height.setter
+    def height(self, value):
+        """Setter for height"""
+        # Will probably need validation later
+        self.__height = value
 
-@property
-def x(self):
-    """ Getter """
-    return self.x
+    @property
+    def x(self):
+        """Getter for x"""
+        return self.__x
 
-@x.setter
-def x(self, value):
-    """ Setter """
-    self.x = value
+    @x.setter
+    def x(self, value):
+        """Setter for x"""
+        # Will probably need validation later
+        self.__x = value
 
-@property
-def y(self):
-    """getter"""
-    return self.y
+    @property
+    def y(self):
+        """Getter for y"""
+        return self.__y
 
-@y.setter
-def y(self, value):
-    """ Setter """
-    self.y = value
+    @y.setter
+    def y(self, value):
+        """Setter for y"""
+        # Will probably need validation later
+        self.__y = value
+
