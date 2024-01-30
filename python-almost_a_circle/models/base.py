83c62d68a00a33a460c251c4405b9b987408583d
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-01-29 20:15:12 -0600

    json string to dictionary

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -39,3 +39,11 @@ class Base:
 
         with open(filename, 'w') as file:
             file.write(json_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or \
+            len(json_string) == 0:
+                return []
+        else:
+            return json.loads(json_string)
