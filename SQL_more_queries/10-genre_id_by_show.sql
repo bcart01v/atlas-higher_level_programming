5d018f653305b7a2a46482afeaf2521f3a37cc05
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-02-17 16:24:09 -0600

    Correction

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -3,7 +3,7 @@
 -- 1 select. 
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows, tv_show_genres
+FROM hbtn_0d_tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
 WHERE tv_shows_genres.genre_id IS NOT NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
