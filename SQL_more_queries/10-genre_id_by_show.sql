5e836efc72dccc8602a1f239242a2d1449992ab9
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-02-17 16:21:05 -0600

    GENRE ID

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- List all shows contained in hbtn_0d_tvshows that have at least 1 genre linked
+-- Display tv_shows.title, tv_show_genres.genre_id
+-- 1 select. 
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows, tv_show_genres
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+WHERE tv_shows_genres.genre_id IS NOT NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
