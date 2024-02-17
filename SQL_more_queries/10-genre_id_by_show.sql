929c3568afb9f787a2798f9c052afa2e7fc720c9
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-02-17 16:29:13 -0600

    Corrections

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,8 +2,8 @@
 -- Display tv_shows.title, tv_show_genres.genre_id
 -- 1 select. 
 
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-WHERE tv_show_genres.genre_id IS NOT NULL
+SELECT tv_shows.title, tv_show_genres.genre_id 
+FROM tv_shows 
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id 
+WHERE tv_show_genres.genre_id IS NOT NULL 
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
