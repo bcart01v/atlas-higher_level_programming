114e25fa58f0de1635146f0ca19f4ae69afb7a35
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-02-17 16:58:11 -0600

    Show only comedies

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,8 @@
+-- only show comedies
+
+SELECT tv_shows.title
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.ID = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.ID
+WHERE tv_genres.Name = 'Comedy'
+ORDER BY tv_shows.title ASC;
