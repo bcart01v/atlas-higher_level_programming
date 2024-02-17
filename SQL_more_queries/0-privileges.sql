a8b7ded23a1267d39850a5c273b3ec74349d4c4a
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-02-17 02:03:02 -0600

    I like this look better

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,7 @@
 -- This script will display all privileges for user user_0d_1 and user_0d_2 on localhost.
 
-SHOW GRANTS FOR 'user_0d_1' @'localhost';
-SHOW GRANTS FOR 'user_0d_2' @'localhost';
+SHOW GRANTS FOR
+'user_0d_1' @'localhost';
+
+SHOW GRANTS FOR
+'user_0d_2' @'localhost';
