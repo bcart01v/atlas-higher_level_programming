4eef9dc84e8f6bf534e91db372640cbf1f8f68b7
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-02-18 02:53:29 -0600

    Having a horrible time getting MySQLdb to work on my mac, lots of compatability issues, but I'm fairly certain this should be correct.

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" 
+This module is used to connect to a MySQL database (hbtn_0e_0_usa)
+and prints all states sorted by state ID
+"""
+
+import MySQLdb
+import sys
+
+def main():
+    """ 
+    Main function that connects to database, and prints.
+    """
+username = sys.argv[1]
+password = sys.argv[2]
+database_name = sys.argv[3]
+
+db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
+
+cursor = db.cursor()
+
+cursor.execute("SELECT * FROM states ORDER BY id ASC")
+results = cursor.fetchall()
+for row in results:
+    print(row)
+
+cursor.close()
+db.close()
+
+if __name__ == "__main__":
+    main()
