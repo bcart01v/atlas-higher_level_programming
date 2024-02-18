6a3c7095db395e9f30473572796b51a17b29bb4d
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-02-18 02:56:44 -0600

    Now this is right

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -13,21 +13,21 @@ def main():
     """ 
     Main function that connects to database, and prints.
     """
-username = sys.argv[1]
-password = sys.argv[2]
-database_name = sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
 
-db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
 
-cursor = db.cursor()
+    cursor = db.cursor()
 
-cursor.execute("SELECT * FROM states ORDER BY id ASC")
-results = cursor.fetchall()
-for row in results:
-    print(row)
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
 
-cursor.close()
-db.close()
+    cursor.close()
+    db.close()
 
 if __name__ == "__main__":
     main()
