d5d04a9f93f52aab9cbe467d3a2150d668f1a48d
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-02-19 14:24:15 -0600

    Ah, I see what you did there! this is what I had originally, but you made me use format. Ha.

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+
+"""
+This module will print only states where the state
+begins with N.
+"""
+
+import sys
+import MySQLdb
+
+
+def main():
+    """
+    Main function. Prints states that begin with N
+    """
+
+    if len(sys.argv) > 1:
+        u = sys.argv[1]
+    else:
+        raise ValueError("Missing Username")
+
+    if len(sys.argv) > 2:
+        p = sys.argv[2]
+    else:
+        raise ValueError("Missing Password")
+
+    if len(sys.argv) > 3:
+        d = sys.argv[3]
+    else:
+        raise ValueError("Missing Database")
+
+# Added a condition to display all if search is null
+    if len(sys.argv) > 4:
+        searchvalue = sys.argv[4]
+    else:
+        searchvalue = None
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=u, passwd=p, db=d)
+
+    cursor = db.cursor()
+
+    if searchvalue is None:
+        query = "SELECT * FROM states ORDER BY id ASC"
+    else:
+        query = "SELECT * FROM states WHERE name LIKE %s " \
+            "ORDER BY id ASC"
+        searchvalue += '%'
+
+
+    cursor.execute(query, (searchvalue,) if searchvalue else None)
+
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
