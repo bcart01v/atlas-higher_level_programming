4c1da6df0ee6a96849d90487745c78977d55c0ae
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-02-19 13:48:30 -0600

    Added condition to show all when our search is blank.
    Trying to minimize line length

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -18,12 +18,30 @@ def main():
     username = param.argv[1]
     password = param.argv[2]
     databasename = param.argv[3]
-    searchvalue = param.argv[4]
+# Added a condition to display all if search is null
+    if len(param.argv) > 4:
+        searchvalue = param.argv[4]
+    else:
+        searchvalue = None
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=databasename)
+    db = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=username,
+    passwd=password,
+    db=databasename
+)
 
     cursor = db.cursor()
-    dbsearchvalue = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(searchvalue)
+
+    if searchvalue is None:
+        query_template = "SELECT * FROM states ORDER BY id ASC"
+    else:
+        query_template = "SELECT * FROM states WHERE name = '{}' " \
+            "ORDER BY id ASC"
+
+    dbsearchvalue = query_template.format(searchvalue)
+
 
     cursor.execute(dbsearchvalue)
 
