af3415dfb0ae86365b4cbbf1b134c752aa57a451
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-02-20 13:45:54 -0600

    Class documentation

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -23,6 +23,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
 
 class City(Base):
+    """
+    This is class documentation
+    """
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
