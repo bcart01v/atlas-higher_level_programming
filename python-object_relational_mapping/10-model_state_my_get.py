b539a8dcb861b27e741e7549de2246dbcf1c5f9a
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-02-20 10:30:28 -0600

    Added a variable for search value. Added error condition if it's blank. Changed print function to only print ID. added count to display a message if none found.

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This prints the state object from the name passed as an argument.
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    mysql_user = sys.argv[1]
+    mysql_pw = sys.argv[2]
+    mysql_database = sys.argv[3]
+    # If the user forgets the state, raise an error.
+    if len(sys.argv) > 4:
+        searchvalue = sys.argv[4]
+    else:
+        raise ValueError("Missing required field: State Name")
+    # Create an engine and connect to the database
+    engine = create_engine(f'mysql+mysqldb://{mysql_user}:{mysql_pw}@localhost:3306/{mysql_database}')
+    Base.metadata.bind = engine
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query for State objects that contain the letter 'a', ordered by states.id
+    statesearch = session.query(State).filter\
+        (State.name.like('%'+searchvalue+'%')).order_by(State.id).all()
+
+    # Print the results
+    count = 0
+    for state in statesearch:
+        print(f"{state.id}")
+        count += 1
+
+    if count == 0:
+        print("Not found")
