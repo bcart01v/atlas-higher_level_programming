40db60d2950fe78dc79a007323b285bf5b3e019a
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-03-24 14:19:00 -0500

    Corrections

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // C is fun (it's not)
-console.log("C is fun");
+console.log('C is fun');
 
 // Python print
-console.log("Python is cool");
+console.log('Python is cool');
 
 // Java print
-console.log("JavaScript is amazing");
+console.log('JavaScript is amazing');
