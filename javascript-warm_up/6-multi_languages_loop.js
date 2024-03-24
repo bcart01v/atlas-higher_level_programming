37f172705a334e9afb974ccf081eca2f6bcf13e7
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-03-24 16:28:36 -0500

    Corrected spelling

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Prints 3 lines, using array and string with a loop.
 
-const arrayPrint = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arrayPrint = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (const line of arrayPrint) {
     console.log(line);
