977d0da80c45bf479085bbb13bc9bf9f8cc25273
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-03-24 16:06:37 -0500

    Feels like this should work and pass.

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 // Now only prints the first argument.
 const FirstArg = process.argv[2];
 
-if (FirstArg == undefined) {
+if (FirstArg === undefined) {
     console.log('No argument');
 } else {
     console.log(FirstArg);
---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Prints first argument and second argument.
+const FirstArg = process.argv[2];
+const SecArg = process.argv[3];
+
+console.log(FirstArg + ' is ' + SecArg);
