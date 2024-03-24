bbb09cdfccf486465c78204a836fbd7172dc1ab2
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-03-24 16:27:53 -0500

    Build array, print it.

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Prints 3 lines, using array and string with a loop.
+
+const arrayPrint = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (const line of arrayPrint) {
+    console.log(line);
+}
