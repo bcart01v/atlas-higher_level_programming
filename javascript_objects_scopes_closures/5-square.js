1d0bcb7d634cb9444d1a002b68b95d04486e2e03
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-03-26 16:26:58 -0500

    Squares now, inherits from Rec.

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,10 @@
+// Assuming Rectangle is defined in 4-rectangle.js
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor(size) {
+    super(size, size);
+  }
+}
+module.exports = Rectangle;
+module.exports = Square;
