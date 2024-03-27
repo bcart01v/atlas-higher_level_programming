498e78a588a73a66b149844413a8bace85192dce
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-03-26 21:59:23 -0500

    Simplified Logic

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,16 +1,7 @@
 #!/usr/bin/node
 // Converter!
-exports.logMe = (function () {
-  let count = 0;
-
-  return function (item) {
-    console.log(`${count}: ${item}`);
-    count++; // Increment the count for the next call
-  };
-})();
-
 exports.converter = function (base) {
   return function (number) {
     return number.toString(base);
-  };
+    };
 };
