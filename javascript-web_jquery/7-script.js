475f10ff15a25a25f430ce8fd6a51a440fcc540f
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-04-01 21:29:58 -0500

    Cleaned up directory. Create script that fetches S

---
+++ javascript-web_jquery/7-script.js
@@ -0,0 +1,7 @@
+// Fetch Character name.
+$(document).ready(function() {
+    $.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function(data) {
+      $('#character').text(data.name);
+    });
+  });
+  
